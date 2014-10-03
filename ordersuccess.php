9c1965cdbd991d9434ddfa60046b86370be248ae
Vermille Saw <veeann@users.noreply.github.com>
2014-10-03 09:32:30 +0800

    Create ordersuccess.php

---
+++ ordersuccess.php
@@ -0,0 +1,34 @@
+<html>
+
+<head>
+<title>Joanie's Cupcakes</title>
+<link rel="stylesheet" href="everywhere.css" />
+<link rel="stylesheet" href="960.css" />
+<link rel="stylesheet" href="reset.css" />
+
+<style type="text/css">
+</style>
+</head>
+
+<body>
+<div class="container">
+
+ 	<div class="header" align="center" >
+ 		</br>
+ 		<a href="index.php">
+ 			<img src="images/logo.jpg" alt="Insert Logo Here" name="Insert_logo" width="169" height="155" id="Insert_logo" style="background-color: #C6D580; display:block;" />
+ 		</a>
+ 		</br>
+ 		<font color="#EE637E">A one-stop cake shop to fulfill your sweet tooth!</font>
+ 		</br>
+ 		</br>
+ 		<p><font color="#FFFFFF">Order Sent. Go <a href="index.php">back</a> to site index.</font></p>
+ 		</br>
+    </div>
+
+  	<div class="footer">Copyright 2014</div>
+
+</div>
+</body>
+
+</html>
