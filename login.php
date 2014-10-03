5a95ae96ee62bab98066ade7162e2dd22f03909b
Vermille Saw <veeann@users.noreply.github.com>
2014-10-03 09:36:18 +0800

    Create login.php

---
+++ login.php
@@ -0,0 +1,36 @@
+<html>
+
+<head>
+<title>Joanie's Cupcakes</title>
+
+<link rel="stylesheet" href="everywhere.css" />
+<link rel="stylesheet" href="960.css" />
+<link rel="stylesheet" href="reset.css" />
+
+<style type="text/css">
+</style>
+</head>
+
+<body>
+
+<div class="container">
+  	<div class="header" align="center" >
+  		</br>
+  		<a href="index.php">
+  			<img src="images/logo.jpg" alt="Insert Logo Here" name="Insert_logo" width="169" height="155" id="Insert_logo" style="background-color: #C6D580; display:block;" />
+  		</a> 
+  		</br>
+  		<form method="post" action="loginprocess.php">
+		<p><font color="#CCCCCC">Username:</font>
+      		<input   border="" type="text" name="logname"/></p>
+    	<p><font color="#CCCCCC">Password: </font>
+    		<input  type="text" name="logpass"/></p>
+    	<button name="login" style="background-color:#EE637E; color:#3C2312; border-radius:50px; height:50px;width:200px"> LOGIN </button>
+		</form>
+  	</div>
+  	<div class="footer">Copyright 2014</div>
+</div>
+
+</body>
+
+</html>
