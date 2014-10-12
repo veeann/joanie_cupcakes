5e5654f59e04a61a2a4b9db5de9bf2e356615251
masterb4ker <jet.opinion@gmail.com>
2014-10-12 21:30:24 +0800

    Update login.php

--- login.php
+++ login.php
@@ -17,18 +17,18 @@
   	<div class="header" align="center" >
   		</br>
   		<a href="index.php">
-  			<img src="images/logo.jpg" alt="Insert Logo Here" name="Insert_logo" width="169" height="155" id="Insert_logo" style="background-color: #C6D580; display:block;" />
+  			<img src="images/logo.png" alt="Insert Logo Here" name="Insert_logo" width="169" height="155" id="Insert_logo" style="background-color: #C6D580; display:block;" />
   		</a> 
   		</br>
   		<form method="post" action="loginprocess.php">
 		<p><font color="#CCCCCC">Username:</font>
       		<input   border="" type="text" name="logname"/></p>
     	<p><font color="#CCCCCC">Password: </font>
-    		<input  type="text" name="logpass"/></p>
+        <label><input type="password" name="logpass" id="myPass" /></label></p>
     	<button name="login" style="background-color:#EE637E; color:#3C2312; border-radius:50px; height:50px;width:200px"> LOGIN </button>
 		</form>
   	</div>
-  	<div class="footer">Copyright 2014</div>
+  	<div class="footer"><img src="images/Logo3.png" width="50" height="50" />Copyright 2014</div>
 </div>
 
 </body>
