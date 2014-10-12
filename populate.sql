586380c2da5c65915077fa5f21b2671a09b60668
Gilo <dancel.gilo@gmail.com>
2014-10-12 21:08:27 +0800

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,25 +1,25 @@
 INSERT INTO Order_t
-VALUES (1, '2014-09-20', 'Yuen', 'WehSing', 'singsing@tinker.com', 09170000001, 'Mocha-frosted cake and with mint sprinkles', 0, 'Rejected');
+VALUES (1, '2014-09-20', 'Yuen', 'WehSing', 'singsing@tinker.com', '09170000001', 'Mocha-frosted cake and with mint sprinkles', 0, 'Rejected');
 
 INSERT INTO Order_t
-VALUES (2, '2014-09-21', 'Sundstein', 'Johan', 'n0tail@secret.com', 09170000002, 'Tofu cake, bitter', 2000, 'Finished');
+VALUES (2, '2014-09-21', 'Sundstein', 'Johan', 'n0tail@secret.com', '09170000002', 'Tofu cake, bitter', 2000, 'Finished');
 
 INSERT INTO Order_t
-VALUES (3, '2014-09-22', 'Ivanov', 'Clement', 'puppey@secret.com', 09170000003, 'One dozen banana-nut muffins with chocolate fudge', 1500, 'Processing');
+VALUES (3, '2014-09-22', 'Ivanov', 'Clement', 'puppey@secret.com', '09170000003', 'One dozen banana-nut muffins with chocolate fudge', 1500, 'Processing');
 
 INSERT INTO Order_t
-VALUES (4, '2014-09-23', 'Taksahomi', 'Kuro', 'kuroky@secret.com', 09170000004, 'Extra large carrot cake, sugarfree', 3000, 'Priced');
+VALUES (4, '2014-09-23', 'Taksahomi', 'Kuro', 'kuroky@secret.com', '09170000004', 'Extra large carrot cake, sugarfree', 3000, 'Priced');
 
 INSERT INTO Order_t
-VALUES (5, '2014-09-24', 'Lille', 'Anders', 'pajkatt@tinker.com', 09170000005, 'Lemon squares with extra lemon seeds', 0, 'Placed');
+VALUES (5, '2014-09-24', 'Lille', 'Anders', 'pajkatt@tinker.com', '09170000005', 'Lemon squares with extra lemon seeds', 0, 'Placed');
 
 INSERT INTO Order_t
-VALUES (6, '2014-09-25', 'Ling', 'Kurtis', 'aui2000@cloud9.com', 09170000006, 'Half dozen red velvet cupcake with extra cream cheese frosting', 0, 'Cancelled');
+VALUES (6, '2014-09-25', 'Ling', 'Kurtis', 'aui2000@cloud9.com', '09170000006', 'Half dozen red velvet cupcake with extra cream cheese frosting', 0, 'Cancelled');
 
 -- -------------------------------------------------------------------------
 
 INSERT INTO Report_t
-VALUES (1, '2014-09-20', '2014-09-25', 6500 );
+VALUES (1, '2014-09-20', '2014-09-25', 6500, 3000, 3500 );
 
 -- -------------------------------------------------------------------------
 
