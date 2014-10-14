716d36afef7ab11cfd5bdbf21fcb12e1e49b7ac6
Gilo <dancel.gilo@gmail.com>
2014-10-14 21:32:41 +0800

    Update sales.php

--- sales.php
+++ sales.php
@@ -108,6 +108,9 @@ button{
               $searchby=$_POST['searchby'];
             if(isset($_POST['search']))
               $searchterm=$_POST['search'];
+
+            if (!preg_match('/[a-zA-Z0-9\s]+/', $searchterm))
+              $searchterm = "-";
             
             if($searchby=="repid")
               $sqlquery.="WHERE report_id = $searchterm ";
